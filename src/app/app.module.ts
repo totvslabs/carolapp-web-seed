09355b5013e38e554abd14e22ca24df0c9bd3ec0
Ingo <ingo@totvslabs.com>
2019-05-29 20:27:17 -0300

    Fixed unit tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthInterceptor } from './services/auth.interceptor';
 
 import { LoginComponent } from './routes/login/login.component';
--- src/app/routes/login/login.component.spec.ts
+++ src/app/routes/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { LoginComponent } from './login.component';
+import { AppModule } from 'src/app/app.module';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +10,7 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [AppModule, HttpClientTestingModule]
     })
     .compileComponents();
   }));
