1930cee3d5389326072927313e8bd09870cf9555
FE <fe01@totvslab.com>
2020-02-19 00:16:17 -0300

    Updating dependencies

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { carol } from '@carol/carol-sdk/lib/carol';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   constructor() {
+  }
+
+  ngOnInit() {
   }
 
 }
