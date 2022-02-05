2a9e919da556e1e16afbd01cba99aa064bffc49a
Carlos Wagner <carlos.afw@gmail.com>
2022-02-05 11:51:25 -0300

    Added connectors sample

--- src/app/sample/components/connectors/connectors.component.ts
+++ src/app/sample/components/connectors/connectors.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { CarolStagingTablesService } from '@totvslabs/carol-app-fe-sdk';
+import { PoTreeViewItem } from '@po-ui/ng-components';
+import { CarolStagingTablesService, Connector, Connectors, StagingTable, StagingTableProperty, StagingTables } from '@totvslabs/carol-app-fe-sdk';
 
 @Component({
   selector: 'app-connectors',
@@ -11,12 +12,60 @@ export class ConnectorsComponent implements OnInit {
   constructor(private stService: CarolStagingTablesService) { }
 
   loadingConnectors: boolean = true;
-  connectors: any;
+  connectors: ConnectorRender[] = [];
     
-  async ngOnInit(
+  async ngOnInit() {
+    const [
+      connectors,
+      stagingTables 
+    ] = await Promise.all([
+      this.stService.getConnectors(),
+      this.stService.getStagingTables()
+    ])
     
-  ) {
-    this.connectors = await this.stService.getConnectors();
+    this.connectors = this.prepareConnectors(connectors, stagingTables)
     this.loadingConnectors = false;
   }  
+
+  private prepareConnectors(connectors: Connectors, stagings: StagingTables): ConnectorRender[] {
+    return Object.entries(connectors)
+      .map(([connectorId, connector]: [string, Connector]) => {
+        const connectorStagings: Array<StagingTable> = stagings[connectorId];
+        const stagingTables: Array<StagingTableRender> = connectorStagings.map((staging) => ({
+            label: `${staging.name} (${staging.id})`,
+            ready: staging.ready ? 'Ready' : 'Not Ready',
+            properties: staging.properties?.map((p) => this.prepareTreeView(p))
+        }))
+
+        return {
+          label: `${connector.name} (${connectorId})`,
+          stagingTables
+        }
+      });
+  }
+
+  private prepareTreeView(stagingProperties: StagingTableProperty): PoTreeViewItem {
+    const treeView = {
+      label: `${stagingProperties.name} - ${stagingProperties.type}`,
+      value: `${stagingProperties.name}__${stagingProperties.type}`,
+      expanded: true
+    } as PoTreeViewItem;
+
+    if (stagingProperties.properties) {
+      treeView.subItems = stagingProperties.properties.map(p => this.prepareTreeView(p));
+    }
+
+    return treeView;
+  }
+}
+
+interface ConnectorRender {
+  label: string;
+  stagingTables: Array<StagingTableRender>,
+}
+
+interface StagingTableRender {
+  label: string;
+  ready: string;
+  properties: Array<PoTreeViewItem>
 }
